b23e1510660ccc4e34754143a88210acf1372edc
Peter Cox <peter.cox@kingfisher.com>
2024-12-02 10:04:48 +0000

    Remove warnings

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,60 +2,60 @@ import kotlin.math.abs
 
 fun main() {
     fun part1(input: List<String>): Int {
-        val rx = Regex("^(\\d*)\\s*(\\d*)\$");
-        var diff  = 0;
+        val rx = Regex("^(\\d*)\\s*(\\d*)\$")
+        var diff  = 0
 
         val l1 = mutableListOf<Int>()
-        val l2 = mutableListOf<Int>();
+        val l2 = mutableListOf<Int>()
 
         for (s in input) {
-            val matches = rx.matchEntire(s);
+            val matches = rx.matchEntire(s)
 
             if (matches != null) {
-                l1.add(matches.groupValues[1].toInt());
-                l2.add(matches.groupValues[2].toInt());
+                l1.add(matches.groupValues[1].toInt())
+                l2.add(matches.groupValues[2].toInt())
             }
             else
             {
-                println("Invalid string");
+                println("Invalid string")
             }
         }
 
-        l1.sort();
-        l2.sort();
+        l1.sort()
+        l2.sort()
 
-        for (i in 0..l1.size - 1)
+        for (i in 0..<l1.size)
         {
-            diff += abs(l1[i] - l2[i]);
+            diff += abs(l1[i] - l2[i])
         }
 
-        return diff;
+        return diff
     }
 
     fun part2(input: List<String>): Int {
-        val rx = Regex("^(\\d*)\\s*(\\d*)\$");
+        val rx = Regex("^(\\d*)\\s*(\\d*)\$")
 
         val l1 = mutableListOf<Int>()
-        val l2 = mutableListOf<Int>();
+        val l2 = mutableListOf<Int>()
 
         for (s in input) {
-            val matches = rx.matchEntire(s);
+            val matches = rx.matchEntire(s)
 
             if (matches != null) {
-                l1.add(matches.groupValues[1].toInt());
-                l2.add(matches.groupValues[2].toInt());
+                l1.add(matches.groupValues[1].toInt())
+                l2.add(matches.groupValues[2].toInt())
             } else {
-                println("Invalid string");
+                println("Invalid string")
             }
         }
 
-        var similarity = 0;
-        for (i in 0..l1.size - 1) {
-            var thisScore = l2.count{it == l1[i]};
-            similarity += l1[i] * thisScore;
+        var similarity = 0
+        for (i in 0..<l1.size) {
+            val thisScore = l2.count{it == l1[i]}
+            similarity += l1[i] * thisScore
         }
 
-        return similarity;
+        return similarity
     }
 
     val input = readInput("Day01")
--- src/Day02.kt
+++ src/Day02.kt
@@ -2,39 +2,39 @@ import kotlin.math.abs
 
 fun main() {
     fun isSafe(n: List<Int>): Boolean {
-        val increasing = n[0] < n[1];
-        var unsafe = false;
+        val increasing = n[0] < n[1]
+        var unsafe = false
         for (i in 0..n.size - 2) {
             if ((n[i] < n[i + 1]) != increasing) {
-                unsafe = true;
-                break;
+                unsafe = true
+                break
             }
 
             val diff = abs(n[i] - n[i + 1])
             if ((diff > 3) || (diff < 1)) {
-                unsafe = true;
-                break;
+                unsafe = true
+                break
             }
         }
         return !unsafe
     }
 
     fun part1(input: List<String>): Int {
-        var safeCounter = 0;
+        var safeCounter = 0
         for (s in input) {
             val t = s.split(" ")
             val n = t.map { it.toInt(); }
 
             if (isSafe(n))
             {
-                safeCounter++;
+                safeCounter++
             }
         }
-        return safeCounter;
+        return safeCounter
     }
 
     fun part2(input: List<String>): Int {
-        var safeCounter = 0;
+        var safeCounter = 0
         for (s in input) {
             val t = s.split(" ")
             val n = t.map { it.toInt(); }
@@ -42,7 +42,7 @@ fun main() {
             // Safe at any level
             if (isSafe(n))
             {
-                safeCounter++;
+                safeCounter++
                 //println("Safe at any level");
             }
             else {
@@ -50,22 +50,22 @@ fun main() {
                 // If at least one is safe
                 // There are much quicker ways to do this, but this is much
                 // easier!
-                var anySafe = false;
+                var anySafe = false
 
                 for (i in n.indices) {
-                    val copy = n.toMutableList();
-                    copy.removeAt(i);
+                    val copy = n.toMutableList()
+                    copy.removeAt(i)
                     if (isSafe(copy)) {
-                        anySafe = true;
+                        anySafe = true
                     }
                 }
 
                 if (anySafe) {
-                    safeCounter++;
+                    safeCounter++
                 }
             }
         }
-        return safeCounter;
+        return safeCounter
     }
 
     val input = readInput("Day02")
