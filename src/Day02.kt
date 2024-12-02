46a0bc2655202d892717c6d0e309d7478479e3ed
Peter Cox <peter.cox@kingfisher.com>
2024-12-02 08:12:54 +0000

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,10 +1,71 @@
+import kotlin.math.abs
+
 fun main() {
+    fun isSafe(n: List<Int>): Boolean {
+        val increasing = n[0] < n[1];
+        var unsafe = false;
+        for (i in 0..n.size - 2) {
+            if ((n[i] < n[i + 1]) != increasing) {
+                unsafe = true;
+                break;
+            }
+
+            val diff = abs(n[i] - n[i + 1])
+            if ((diff > 3) || (diff < 1)) {
+                unsafe = true;
+                break;
+            }
+        }
+        return !unsafe
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var safeCounter = 0;
+        for (s in input) {
+            val t = s.split(" ")
+            val n = t.map { it.toInt(); }
+
+            if (isSafe(n))
+            {
+                safeCounter++;
+            }
+        }
+        return safeCounter;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var safeCounter = 0;
+        for (s in input) {
+            val t = s.split(" ")
+            val n = t.map { it.toInt(); }
+
+            // Safe at any level
+            if (isSafe(n))
+            {
+                safeCounter++;
+                //println("Safe at any level");
+            }
+            else {
+                // Loop through each one, removing an element and seeing
+                // If at least one is safe
+                // There are much quicker ways to do this, but this is much
+                // easier!
+                var anySafe = false;
+
+                for (i in n.indices) {
+                    val copy = n.toMutableList();
+                    copy.removeAt(i);
+                    if (isSafe(copy)) {
+                        anySafe = true;
+                    }
+                }
+
+                if (anySafe) {
+                    safeCounter++;
+                }
+            }
+        }
+        return safeCounter;
     }
 
     val input = readInput("Day02")
---
+++ src/Day03.kt
@@ -0,0 +1,15 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size;
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size;
+    }
+
+    val input = readInput("Day03")
+    part1(input).println()
+    part2(input).println()
+}
