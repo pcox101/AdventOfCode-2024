af0c6c2fc20e1846155f2a837042599606bb918a
Peter Cox <peter.cox@kingfisher.com>
2024-12-03 08:06:20 +0000

    Day 03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,49 @@
 import kotlin.math.abs
 
 fun main() {
+    fun getTotal(rx: Regex, line: String): Int {
+        val matches = rx.findAll(line);
+        var total = 0;
+        for (match in matches) {
+            total += match.groupValues[1].toInt() * match.groupValues[2].toInt()
+        }
+        return total
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size;
+        val rx = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        var total = 0;
+        for (line in input) {
+            total += getTotal(rx, line)
+        }
+
+        return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size;
+        val rx = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+        val doordont = Regex("do\\(\\)|don't\\(\\)")
+
+        var inMult = true;
+        var total = 0
+
+        for (line in input) {
+            val matches = doordont.findAll(line).toList();
+            var split = doordont.split(line);
+
+            for (i in 0..<split.size) {
+                // Get the match that starts this part
+                if (i != 0)
+                {
+                    inMult = matches[i - 1].value == "do()"
+                }
+                if (inMult) {
+                    total += getTotal(rx, split[i])
+                }
+            }
+        }
+
+        return total
     }
 
     val input = readInput("Day03")
---
+++ src/Day04.kt
@@ -0,0 +1,11 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
