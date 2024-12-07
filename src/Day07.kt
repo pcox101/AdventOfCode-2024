36f4b49d1b991891043e4ca949b9116d72e48654
Peter Cox <peter.cox@kingfisher.com>
2024-12-07 10:31:40 +0000

    Day 07

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,13 +1,88 @@
+import java.math.BigInteger
 import kotlin.time.measureTime
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun canParsePart1(result: BigInteger, list: List<BigInteger>) : Boolean
+    {
+        if (list.size == 1)
+        {
+            if (list[0] != result)
+                return false
+            else
+                return true
+        }
+        if (list[0] > result) return false
+
+        val newList = list.subList(2, list.size)
+        val newTimesList = mutableListOf(list[0] * list[1])
+        newTimesList.addAll(newList)
+        val newAddList = mutableListOf(list[0] + list[1])
+        newAddList.addAll(newList);
+
+        return canParsePart1(result, newTimesList) || canParsePart1(result, newAddList)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): BigInteger {
+        val rx = Regex("^(\\d*): ((\\d* ?)*)\$")
+        var total = 0.toBigInteger();
+        for (s in input) {
+            val matches = rx.matchEntire(s)
+            if (matches != null) {
+                val result = matches.groupValues[1].toBigInteger()
+                val list = matches.groupValues[2].split(" ").map { it.toBigInteger() }
+
+                if (canParsePart1(result, list)) {
+                    total += result
+                }
+            } else {
+                println("Invalid String")
+            }
+        }
+        return total
+    }
+
+    fun canParsePart2(result: BigInteger, list: List<BigInteger>) : Boolean
+    {
+        if (list.size == 1)
+        {
+            if (list[0] != result)
+                return false
+            else
+                return true
+        }
+        if (list[0] > result) return false
+
+        val newList = list.subList(2, list.size)
+        val newTimesList = mutableListOf(list[0] * list[1])
+        newTimesList.addAll(newList)
+        val newAddList = mutableListOf(list[0] + list[1])
+        newAddList.addAll(newList)
+        val newConcatenateList = mutableListOf((list[0].toString() + list[1].toString()).toBigInteger())
+        newConcatenateList.addAll(newList)
+
+        return canParsePart2(result, newTimesList)
+                    || canParsePart2(result, newAddList)
+                    || canParsePart2(result, newConcatenateList)
+    }
+
+    fun part2(input: List<String>): BigInteger {
+        val rx = Regex("^(\\d*): ((\\d* ?)*)\$")
+        var total = 0.toBigInteger();
+        for (s in input) {
+            val matches = rx.matchEntire(s)
+            if (matches != null) {
+                val result = matches.groupValues[1].toBigInteger()
+                val list = matches.groupValues[2].split(" ").map { it.toBigInteger() }
+
+                if (canParsePart2(result, list)) {
+                    total += result
+                }
+            } else {
+                println("Invalid String")
+            }
+        }
+        return total
     }
 
     val input = readInput("Day07")
