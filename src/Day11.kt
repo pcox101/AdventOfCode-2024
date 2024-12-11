3f007ae604990d838aad035bb3db9db1df33b7c6
Peter Cox <peter.cox@kingfisher.com>
2024-12-11 16:38:03 +0000

    Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,13 +1,60 @@
+import java.math.BigInteger
 import kotlin.time.measureTime
 
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.size
+    val cache = mutableMapOf<Pair<Long, Long>,Long>()
+
+    fun calculateBlink(stone: Long, blinksRemaining: Long): Long
+    {
+        if (blinksRemaining == 0L) return 1L
+
+        val cacheEntry = cache[Pair(stone, blinksRemaining)]
+        if (cacheEntry != null)
+            return cacheEntry
+
+        val stoneAsString = stone.toString()
+        if (stone == 0L) {
+            val v = calculateBlink(1, blinksRemaining - 1 )
+            cache.put(Pair(1, blinksRemaining - 1), v)
+            return v
+        }
+        else if (stoneAsString.length.mod(2) == 0) {
+            val half = stoneAsString.length / 2
+            val s1 = stoneAsString.substring(0, half).toLong()
+            val v1 = calculateBlink(s1, blinksRemaining - 1)
+            cache.put(Pair(s1, blinksRemaining - 1), v1)
+            val s2 = stoneAsString.substring(half, stoneAsString.length).toLong()
+            val v2 = calculateBlink(s2, blinksRemaining - 1)
+            cache.put(Pair(s2, blinksRemaining - 1), v2)
+            return v1 + v2
+        }
+        else {
+            val v = calculateBlink(stone * 2024, blinksRemaining - 1)
+            cache.put(Pair(stone * 2024, blinksRemaining - 1), v)
+            return v
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        val stones = input[0].split(" ").map { it.toLong() }
+
+        var v = 0L
+        for (stone in stones) {
+            v += calculateBlink(stone, 25)
+        }
+        return v
+    }
+
+    fun part2(input: List<String>): Long {
+
+        val stones = input[0].split(" ").map { it.toLong() }
+
+        var v = 0L
+        for (stone in stones) {
+            v += calculateBlink(stone, 75)
+        }
+        return v
     }
 
     val input = readInput("Day11")
