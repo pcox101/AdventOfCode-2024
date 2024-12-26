52d06b7a640f970b6da95d76ab9475da5e12062f
Peter Cox <peter.cox@kingfisher.com>
2024-12-26 21:17:36 +0000

    Day 24 - part 2

--- src/Day24.kt
+++ src/Day24.kt
@@ -77,14 +77,16 @@ fun main() {
             val entry = q.remove()
 
             if (entry.second == 0) continue
-            if (entry.first.startsWith("x") || entry.first.startsWith("y")) continue
+            //if (entry.first.startsWith("x") || entry.first.startsWith("y")) continue
             if (!entry.first.startsWith("z")) {
                 o.add(entry.first)
             }
 
             val g = gateOutputMap[entry.first]
-            q.add(Pair(g!!.input1, entry.second - 1))
-            q.add(Pair(g.input2, entry.second - 1))
+            if (g != null) {
+                q.add(Pair(g.input1, entry.second - 1))
+                q.add(Pair(g.input2, entry.second - 1))
+            }
         }
 
         return o
@@ -93,6 +95,10 @@ fun main() {
     fun part2(input: List<String>): String {
         val inputRx = Regex("^([\\d|\\w]*): ([0|1])\$")
         val gateRx = Regex("^([\\d|\\w]*) (AND|XOR|OR) ([\\d|\\w]*) -> ([\\d|\\w]*)\$")
+
+        // I created these by hand by analysing the maps on a piece of paper...
+        val swaps = mapOf(Pair("gdd","z05"),Pair("z05","gdd"),Pair("cwt","z09"),Pair("z09","cwt"),Pair("jmv","css"),Pair("css","jmv"),Pair("pqt","z37"),Pair("z37","pqt"))
+        //val swaps = mapOf<String,String>()
 
         val inputMap = mutableMapOf<String, Boolean>()
 
@@ -104,8 +110,15 @@ fun main() {
                 inputMap[inputMatches.groupValues[1]] = false // (inputMatches.groupValues[2] == "1")
             }
             if (gateMatches != null) {
-                val g = Gate(gateMatches.groupValues[1], gateMatches.groupValues[3], gateMatches.groupValues[4], gateMatches.groupValues[2])
-                gateOutputMap[gateMatches.groupValues[4]] = g
+
+                var output = gateMatches.groupValues[4]
+                if(swaps.containsKey(output))
+                {
+                    output = swaps[output]!!
+                }
+
+                val g = Gate(gateMatches.groupValues[1], gateMatches.groupValues[3], output, gateMatches.groupValues[2])
+                gateOutputMap[output] = g
             }
         }
 
@@ -124,6 +137,7 @@ fun main() {
             )
 
             var hasError = false
+            var hasCarryError = false
             for (entry in testArray) {
 
                 memo.clear()
@@ -133,24 +147,33 @@ fun main() {
                 val output = getGateOutputValue("z$n")
                 val carry = getGateOutputValue("z$nPlus1")
 
-                if ((output != entry.second.first) || (carry != entry.second.second)) {
+                if (output != entry.second.first)  {
                     hasError = true
+                }
+                if  (carry != entry.second.second) {
+                    hasCarryError = true
                 }
             }
 
-            if (hasError) {
-                println("Error at z$n.")
+            if ((hasError) || (hasCarryError)) {
+                if (hasError) println("Error at z$n.")
+                else println("Carry error at z$n.")
                 // Print out the values of the gates leading to this one
                 val v = getChildren("z$n", 3)
 
                 for (g in v) {
-                    println(gateOutputMap[g]!!)
+                    val v = gateOutputMap[g]
+                    println(v)
                 }
             }
         }
 
+        // Get our swaps and sort them alphabetically
+        val o = swaps.keys.sorted()
+        val sb = StringBuilder()
+        for (k in o) sb.append("$k,")
 
-        return ""// (x + y).toString()
+        return sb.toString()
     }
 
     val input = readInput("Day24")
